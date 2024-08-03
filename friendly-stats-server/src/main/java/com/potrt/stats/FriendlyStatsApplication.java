262c8788267f2a41a2b23879f68982aa00dc21cc
sonicbigboom <sbb-github@potrt.com>
2024-08-03 01:24:37 -0400

    Created server with Spring Boot + Gradle from 'https://start.spring.io/'.

---
+++ friendly-stats-server/src/main/java/com/potrt/stats/FriendlyStatsApplication.java
@@ -0,0 +1,13 @@
+package com.potrt.stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendlyStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendlyStatsApplication.class, args);
+	}
+
+}
---
+++ friendly-stats-server/src/test/java/com/potrt/stats/FriendlyStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potrt.stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendlyStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
