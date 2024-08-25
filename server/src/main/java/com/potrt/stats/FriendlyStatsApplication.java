a527627517dcdc1f42475e556d707dff33a95e9e
sonicbigboom <sbb-github@potrt.com>
2024-08-25 06:00:30 -0400

    Added etc documentation.

--- server/src/main/java/com/potrt/stats/FriendlyStatsApplication.java
+++ server/src/main/java/com/potrt/stats/FriendlyStatsApplication.java
@@ -4,9 +4,11 @@ package com.potrt.stats;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/** The {@link FriendlyStatsApplication} contains the Friendly Stats API entrypoint. */
 @SpringBootApplication
 public class FriendlyStatsApplication {
 
+  /** The Friendly Stats API entrypoint. */
   public static void main(String[] args) {
     SpringApplication.run(FriendlyStatsApplication.class, args);
   }
--- server/src/main/java/com/potrt/stats/config/TitlePhysicalNamingStrategy.java
+++ server/src/main/java/com/potrt/stats/config/TitlePhysicalNamingStrategy.java
@@ -6,6 +6,10 @@ import org.hibernate.boot.model.naming.Identifier;
 import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
 import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
 
+/**
+ * The application database uses a title case naming convention. So hibernate should default to
+ * converting application table/column names to title case.
+ */
 public class TitlePhysicalNamingStrategy implements PhysicalNamingStrategy {
 
   @Override
@@ -38,6 +42,12 @@ public class TitlePhysicalNamingStrategy implements PhysicalNamingStrategy {
     return capitalize(identifier);
   }
 
+  /**
+   * Capitalizes the first letter of an identifier.
+   *
+   * @param identifier The identifier.
+   * @return A capitalized identifier.
+   */
   private Identifier capitalize(final Identifier identifier) {
     if (identifier == null) {
       return null;
